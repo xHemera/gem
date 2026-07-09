76bc8740db03c28a8f9e1e2f9d3ed244613d2e6b
hemera <hemera@debian.home>
2026-07-09 09:42:46 +0200

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import netlify from '@astrojs/netlify';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [react()]
+});
